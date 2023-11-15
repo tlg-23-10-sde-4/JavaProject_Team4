cbb6e4bed8e856d455eb3c9d816867e9441e4d22
Lawliet <Vitalii.Chibizov@tlgcohort.com>
2023-11-15 09:53:45 -0800

    Card class + test class to it

---
+++ src/com/clashofcards/Card.java
@@ -0,0 +1,70 @@
+package com.clashofcards;
+
+public class Card {
+    private Integer index;
+    private String name;
+    private Integer strength;
+    private Integer toughness;
+
+
+    //  constructors
+    public Card(){
+    }
+
+    public Card(Integer index, String name, Integer strength, Integer toughness){
+        setIndex(index);
+        setName(name);
+        setStrength(strength);
+        setToughness(toughness);
+    }
+
+
+
+    //  accessor method
+    public Integer getIndex() {
+        return index;
+    }
+
+    public void setIndex(Integer index) {
+        this.index = index;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getStrength() {
+        return strength;
+    }
+
+    public void setStrength(Integer strength) {
+        this.strength = strength;
+    }
+
+    public Integer getToughness() {
+        return toughness;
+    }
+
+    public void setToughness(Integer toughness) {
+        this.toughness = toughness;
+    }
+
+    @Override
+    public String toString(){
+        return String.format(
+                "---------------------\n" +
+                "| #: %s       %s |\n" +
+                "|                   |\n" +
+                "|                   |\n" +
+                "|                   |\n" +
+                "|                   |\n" +
+                "|                   |\n" +
+                "| S: %s    |    T: %s |\n" +
+                "---------------------"
+        ,getIndex(),getName(),getStrength(),getToughness());
+    }
+}
---
+++ test/CardTest.java
@@ -0,0 +1,16 @@
+/*
+ * Class to test Card and it's methods
+ */
+
+import com.clashofcards.Card;
+import org.junit.Test;
+
+public class CardTest extends Card {
+    Card card = new Card(3, "Goblin", 2, 1);
+
+    @Test
+    public void showCard(){
+        System.out.println(card);
+    }
+
+}
