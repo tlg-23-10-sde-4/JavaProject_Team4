75de2e4ba54b5eb0335342d664d957fb0873ce19
SSPENELOPE <typoepping@gmail.com>
2023-11-15 12:54:19 -0500

    Added the stub methods for the game logic and need compenents for the game.

--- src/com/clashofcards/CardGame.java
+++ src/com/clashofcards/CardGame.java
@@ -1,4 +1,68 @@
 package com.clashofcards;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CardGame {
+    Player player = new Player();
+    Player enemy = new Ai();
+    List<Card> playerBattleField = new ArrayList<>();
+    List<Card> enemyBattleField = new ArrayList<>();
+
+    // CTOR's
+    public CardGame() {}
+
+    public CardGame(Player player, Player enemy, List<Card> playerBattleField, List<Card> enemyBattleField) {
+        this.player = player;
+        this.enemy = enemy;
+        this.playerBattleField = playerBattleField;
+        this.enemyBattleField = enemyBattleField;
+    }
+
+    // Business Methods/Game Logic
+    private static void playerAttackPhase(Player player, Ai enemy) {
+        // TODO: add game logic for player attack phase
+    }
+
+    private static void playerDefensePhase(Player player, Ai enemy) {
+        // TODO: add logic for player defense phase
+    }
+
+    private static void initalizeCards() {
+        // TODO: initalize player decks if we cannot make this work in the card class
+    }
+
+
+    // Getters Setters
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public Player getEnemy() {
+        return enemy;
+    }
+
+    public void setEnemy(Player enemy) {
+        this.enemy = enemy;
+    }
+
+    public List<Card> getPlayerBattleField() {
+        return playerBattleField;
+    }
+
+    public void setPlayerBattleField(List<Card> playerBattleField) {
+        this.playerBattleField = playerBattleField;
+    }
+
+    public List<Card> getEnemyBattleField() {
+        return enemyBattleField;
+    }
+
+    public void setEnemyBattleField(List<Card> enemyBattleField) {
+        this.enemyBattleField = enemyBattleField;
+    }
 }
--- src/com/clashofcards/client/MainClass.java
+++ src/com/clashofcards/client/MainClass.java
@@ -1,7 +1,12 @@
 package com.clashofcards.client;
 
+import com.clashofcards.CardGame;
+
 public class MainClass {
     public static void main(String[] args) {
-
+        CardGame cardGame = new CardGame();
+        while(> 0 || enemy.getHealth() > 0) {
+            // TODO: game Logic and methods here
+        }
     }
 }
