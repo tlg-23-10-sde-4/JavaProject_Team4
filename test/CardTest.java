0b273986e250962b09b3877ba88494e7c0be257a
Lawliet <Vitalii.Chibizov@tlgcohort.com>
2023-11-20 05:33:21 -0800

    ASCII docs updated and method to show is done. Cards updated

--- test/CardTest.java
+++ test/CardTest.java
@@ -14,12 +14,12 @@ public class CardTest extends Card {
 
     @Before
     public void init() {
-        newDeckList.add(new Card(11, "Demon", 2, 1));
-        newDeckList.add(new Card(12, "Fairy", 2, 1));
-        newDeckList.add(new Card(13, "Goblin", 2, 1));
-        newDeckList.add(new Card(14, "Skeleton", 2, 1));
-        newDeckList.add(new Card(15, "Vampire", 2, 1));
-        newDeckList.add(new Card(16, "Zombie", 2, 1));
+        newDeckList.add(new Card(11, "Beast", 2, 1));
+        newDeckList.add(new Card(12, "Reptile", 2, 1));
+        newDeckList.add(new Card(13, "Spider", 2, 1));
+        newDeckList.add(new Card(14, "Rodent", 2, 1));
+        newDeckList.add(new Card(15, "Minotaur", 2, 1));
+        newDeckList.add(new Card(16, "Phoenix", 2, 1));
     }
 
     @Test
